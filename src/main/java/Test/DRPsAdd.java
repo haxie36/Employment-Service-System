47aa46cf8d566df5a3d79a81faaabf6ce479f6ca
haxie36 <sindikad50@gmail.com>
2026-03-14 19:28:12 +0200

    Yeah, I actually only reworked one of the drivers... Now its both reworked!

--- src/main/java/Test/DRPsAdd.java
+++ src/main/java/Test/DRPsAdd.java
@@ -1,6 +1,7 @@
 package main.java.Test;
 
 import main.java.Profile;
+import main.java.Profiles;
 
 import java.util.Arrays;
 
@@ -13,31 +14,37 @@ public class DRPsAdd {
         print(profiles1);
 
         // Test 2
-        Profiles profiles2 = new Profiles(new String[]{"1"});
+        Profiles profiles2 = new Profiles(new Profile[]{new Profile("1")});
         Profile profile2 = new Profile("1");
         profiles2.add(profile2);
         print(profiles2);
 
         // Test 3
-        Profiles profiles3 = new Profiles(new String[]{"1","2","3"});
+        Profiles profiles3 = new Profiles();
+        for (int i = 0; i<3; i++){
+            profiles3.add(new Profile(Integer.toString(i)));
+        }
         Profile profile3 = new Profile("3");
         profiles3.add(profile3);
         print(profiles3);
 
         // Test 4
-        Profiles profiles4 = new Profiles(new String[]{"1","2","3"});
+        Profiles profiles4 = new Profiles();
+        for (int i = 0; i<3; i++){
+            profiles4.add(new Profile(Integer.toString(i)));
+        }
         Profile profile4 = new Profile("4");
         profiles4.add(profile4);
         print(profiles4);
 
         // Test 5
-        Profiles profiles5 = new Profiles(new String[]{"1"});
+        Profiles profiles5 = new Profiles(new Profile[]{new Profile("1")});
         Profile profile5 = new Profile("2");
         profiles5.add(profile5);
         print(profiles5);
     }
 
     private static void print(Profiles profiles) {
-        System.out.println(Arrays.toString(profiles.getClientIds()));
+        System.out.println(Arrays.toString(profiles.getProfiles()));
     }
 }
