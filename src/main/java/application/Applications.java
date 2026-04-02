be617932d7962fec280f6d989d582c6470440f01
haxie36 <sindikad50@gmail.com>
2026-04-02 08:47:50 +0300

    + So umm... I copied and pasted the code, sooo... "Application[] applications" were named as "vacancies"

--- src/main/java/application/Applications.java
+++ src/main/java/application/Applications.java
@@ -2,43 +2,43 @@ package application;
 
 import common.Application;
 
-public class Applications {
-    private Application[] vacancies;
+public class Applications { //A collection of Applications
+    private Application[] applications;
 
     public Applications() {
-        vacancies = new Application[0];
+        applications = new Application[0];
     }
-    public Applications(Application[] vacancies) {
-        this.vacancies = vacancies;
+    public Applications(Application[] applications) {
+        this.applications = applications;
     }
 
     public boolean add(Application application) {
         String id = application.getId();
         boolean exists = false;
 
-        for (int i=0; i<vacancies.length; i++) {
-            if (id.equals(vacancies[i].getId())) {
+        for (int i=0; i<applications.length; i++) {
+            if (id.equals(applications[i].getId())) {
                 exists = true;
                 break;
             }
         }
 
         if (!exists) {
-            Application[] newApplications = new Application[vacancies.length+1];
+            Application[] newApplications = new Application[applications.length+1];
 
-            for (int i=0; i<vacancies.length; i++) {
-                newApplications[i] = vacancies[i];
+            for (int i=0; i<applications.length; i++) {
+                newApplications[i] = applications[i];
             }
 
-            newApplications[vacancies.length] = application;
-            vacancies=newApplications;
+            newApplications[applications.length] = application;
+            applications=newApplications;
         }
         return !exists;
     }
 
     public Application getApplication(String id) {
-        for (int i=0; i<vacancies.length; i++) {
-            Application application = vacancies[i];
+        for (int i=0; i<applications.length; i++) {
+            Application application = applications[i];
 
             if (id.equals(application.getId())) {
                 return application;
@@ -47,22 +47,22 @@ public class Applications {
         return null;
     }
 
-    public Application[] getApplications() { return vacancies; }
-    public void clear(){ vacancies = new Application[0]; }
+    public Application[] getApplications() { return applications; }
+    public void clear(){ applications = new Application[0]; }
 
     public boolean delete(Application application) {
         String id = application.getId();
         return delete(id);
     }
     public boolean delete(String id){
-        for (int i=0; i<vacancies.length; i++) {
-            if (id.equals(vacancies[i].getId())) {
-                Application[] newApplications = new Application[vacancies.length-1];
-                for (int j=0; j<vacancies.length; j++) {
+        for (int i=0; i<applications.length; i++) {
+            if (id.equals(applications[i].getId())) {
+                Application[] newApplications = new Application[applications.length-1];
+                for (int j=0; j<applications.length; j++) {
                     if (j==i) continue;
-                    newApplications[j] = vacancies[i];
+                    newApplications[j] = applications[i];
                 }
-                vacancies=newApplications;
+                applications=newApplications;
                 return true;
             }
         }
